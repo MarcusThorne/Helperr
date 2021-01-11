6e842438493164072b8ac341a4f4cdfb4c0a356a
Marcus Thorne <marcus_thorne475@hotmail.com>
2021-01-11 21:32:13 +0000

    added the professions cards under the featured, chnaged the title at the top of the page and created a transparent navbar when att he top. Also added another parallax

--- app/javascript/components/navbarTransparent.js
+++ app/javascript/components/navbarTransparent.js
@@ -1,9 +1,11 @@
 const navbarTransparent = () => {
   window.addEventListener('scroll', function() {
     let navbar = document.querySelector('.navbar-container')
+    let collapse = document.querySelector(".content-collapsed")
     let windowPosition = window.scrollY > 80
-
     navbar.classList.toggle('scrolling-active', windowPosition)
+    collapse.classList.toggle('scrolling-active', windowPosition)
+    console.log("Scrolling class active")
   })
 }
 
